b9cf2c0892d985753f3bd979ed746c86c2dcc438
usager <usager@10.0.2.15>
2019-11-22 14:30:14 -0500

    //version1.0

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -16,6 +16,7 @@ public class Test2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Bonjour"+args[1]+", "+args[0]);
     }
     
 }
